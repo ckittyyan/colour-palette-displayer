1cd498e200939675ea174eca1e5efe6fc16289c2
ykhello <ykhello@student.ubc.ca>
2023-03-26 02:42:18 -0700

    converting to use hooks instead of class component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,85 @@
 import './App.css';
 import {PhotoContainer} from "./PhotoContainer"
-import { Component } from 'react';
+import { useState, useEffect, Component } from 'react';
+import { Photo } from './Photo';
 
 // code referenced from https://medium.com/@yahtzeemoomtaz/fetch-from-an-api-and-display-some-pictures-react-4de2a027eda7
 // https://www.linkedin.com/learning/react-js-essential-training-14836121/
 // https://www.linkedin.com/learning/react-js-building-an-interface-8551484/
 // https://stackoverflow.com/questions/39019094/reactjs-get-json-object-data-from-an-url
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      photos: [],
-      loading: true,
-      hasError: false
-    };
-  }
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       photos: [],
+//       loading: true,
+//       hasError: false
+//     };
+//   }
 
-componentDidMount() {
-  fetch("http://jsonplaceholder.typicode.com/photos?albumId=1")
-  .then(response => {
-    //console.log("response", response);
-    if (!response.ok) {
-      this.setState( {hasError: true})
-      throw new Error("There was an error grabbing images");
-    }
-    return response.json()
-    .then(allData => {
-      this.setState( { photos: allData,
-      loading: false });
-    })
-    .catch((err) => {
-      this.setState( {hasError: true})
-      throw new Error(err.message);
-    });
-  })
-}
+// componentDidMount() {
+//   fetch("http://jsonplaceholder.typicode.com/photos?albumId=1")
+//   .then(response => {
+//     //console.log("response", response);
+//     if (!response.ok) {
+//       this.setState( {hasError: true})
+//       throw new Error("There was an error grabbing images");
+//     }
+//     return response.json()
+//     .then(allData => {
+//       this.setState( { photos: allData,
+//       loading: false });
+//     })
+//     .catch((err) => {
+//       this.setState( {hasError: true})
+//       throw new Error(err.message);
+//     });
+//   })
+// }
 
-render() {
-  const {loading, hasError} = this.state;
-  if (hasError) {
-    return <p>Uh oh... an error has occurred! </p>
-  }
-  return (
-    <div>
-    { loading ? (
-      <div> Loading images... </div>
-    ) : (
+// render() {
+//   const {loading, hasError} = this.state;
+//   if (hasError) {
+//     return <p>Uh oh... an error has occurred! </p>
+//   }
+//   return (
+//     <div>
+//     { loading ? (
+//       <div> Loading images... </div>
+//     ) : (
     
-      <PhotoContainer photos={this.state.photos} />
+//       <PhotoContainer photos={this.state.photos} />
       
     
-  )}
-  </div>
-  );
-}
+//   )}
+//   </div>
+//   );
+// }
+// }
+
+function App() {
+  const [photos, setPhotos] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://jsonplaceholder.typicode.com/photos?albumId=1")
+    .then((response) => response.json())
+    .then(setPhotos)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
+  if (loading) return <h1>Loading images...</h1>
+  if (error) return <div>Uh Oh... an error occurred: {error.message}</div>
+  if (!photos) return null;
+
+  return (
+    <PhotoContainer photos={photos}/>
+  )
+
 }
 
 export default App;
