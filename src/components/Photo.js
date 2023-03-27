28c431322a9e494aaf087992ea2f1fc163561777
ykhello <ykhello@student.ubc.ca>
2023-03-26 19:20:59 -0700

    removing redundant comments

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -3,12 +3,8 @@ export const Photo = (props) => {
     return (
         <div key={props.id} className="photo-item">
             
-            
-           
             <img src={props.url} alt={props.title} loading="lazy"/>
             <div className="photo-title"> {props.title} </div>
-            {/* <div>ID: {props.id}</div>
-            <div>title: {props.title}</div> */}
            
             
         </div>
